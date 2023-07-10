343e44428e8386c44227581d92f5a8922a7077e1
DandiRizkyy <dandirizkyy94@gmail.com>
2023-07-10 22:33:10 +0700

    update post

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { NoteModule } from './note/dto/note.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [AuthModule, UserModule, NoteModule],
+  imports: [AuthModule, UserModule, NoteModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
